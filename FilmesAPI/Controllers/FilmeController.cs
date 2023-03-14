727a7afee251c130748b4ed12a5d51cf85be7f47
Bruno de Avila <brunomunao@outlook.com>
2023-03-13 22:14:35 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine($"Filme: {filme.Titulo}");
+        Console.WriteLine($"Ano: {filme.Ano}");
+    }
+}
