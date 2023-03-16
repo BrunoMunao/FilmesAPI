284539eeb142c33d8844007b182f1a7285194d8e
Bruno de Avila <brunomunao@outlook.com>
2023-03-15 22:21:24 -0300

    Ajustes dos métodos.

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -9,18 +9,27 @@ namespace FilmesAPI.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
-    public void AdicionaFilme([FromBody] Filme filme)
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine($"Filme: {filme.Titulo}");
-        Console.WriteLine($"Ano: {filme.Ano}");
+        return CreatedAtAction(nameof(RetornaFilmePorId), new { id = filme.Id }, filme);
     }
 
     [HttpGet]
-    public IEnumerable<Filme> RetornaFilmes()
+    public IEnumerable<Filme> RetornaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
-        return filmesAppend();
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RetornaFilmePorId(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
     }
 }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -4,20 +4,26 @@ namespace FilmesAPI.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
+
     [Required(ErrorMessage = "O titúlo é obrigatório!")]
     [MaxLength(255, ErrorMessage = "O tamanho do título não pode exceder 255 caracteres.")]
     public string Titulo { get; set; }
+
     [Required(ErrorMessage = "O ano é obrigatório!")]
     [MaxLength(4, ErrorMessage = "O tamanho do ano não pode exceder 255 caracteres.")]
     public string Ano { get; set; }
+
     [Required(ErrorMessage = "O diretor é obrigatório!")]
     [MaxLength(255, ErrorMessage = "O tamanho do nomde do diretor não pode exceder 255 caracteres.")]
     public string Diretor { get; set; }
+
     [Required(ErrorMessage = "O gênero é obrigatório!")]
     [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres.")]
     public string Genero { get; set; }
+
     [Required(ErrorMessage = "A duração é obrigatória!")]
     [Range(70, 600, ErrorMessage = "O filme tem que ter tempo entre 70 e 600 minutos.")]
     public int Duracao { get; set; }
-
+    
 }
